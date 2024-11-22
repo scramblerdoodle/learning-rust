160a1d839783ea759ef6725c5c7c5561825fda98
Pedro Orlando <porlando@riseup.net>
2024-11-22 18:11:47 -0300

    Advent of Code Day 1 Part 2: it's wrong lol

--- advent_of_code_23/src/day1.rs
+++ advent_of_code_23/src/day1.rs
@@ -14,52 +14,184 @@ In this example, the calibration values of these four lines are 12, 38, 15, and 
 Consider your entire calibration document. What is the sum of all of the calibration values?
 */
 
-//use std::{collections::HashSet, fs};
-//
-//const NUMBER_NAMES: HashSet<&str> = HashSet::from([
-//    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero",
-//]);
-//
-//fn extract_digits_v2(strs: Vec<String>) -> u32 {
-//    let mut nums: Vec<u32> = vec![];
-//
-//    for s in strs {
-//        println!("{s}");
-//
-//        let mut chars = s.chars();
-//        let mut chars_rev = s.chars().rev();
-//
-//        let first_digit = loop {
-//            let c = chars.next().expect("No digits were found.");
-//
-//            if c.is_numeric() {
-//                break c;
-//            }
-//        };
-//
-//        let last_digit = loop {
-//            let c = chars_rev.next().expect("No digits were found.");
-//
-//            if c.is_numeric() {
-//                break c;
-//            }
-//        };
-//
-//        let number: u32 = String::from_iter([first_digit, last_digit])
-//            .parse()
-//            .unwrap();
-//        nums.push(number);
-//    }
-//
-//    nums.iter().sum::<u32>()
-//}
+use std::collections::{HashMap, HashSet};
+use std::fs;
+use std::sync::OnceLock;
 
+const NUMBERS: [&str; 10] = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+const NUMBERS_REV: [&str; 10] = [
+    "orez", "ene", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin",
+];
+
+// {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+macro_rules! make_number_set {
+    ( $func_name:ident, $x:expr ) => {
+        fn $func_name() -> &'static HashSet<&'static str> {
+            static HASHMAP: OnceLock<HashSet<&'static str>> = OnceLock::new();
+            HASHMAP.get_or_init(|| HashSet::from_iter($x))
+        }
+    };
+}
+
+macro_rules! make_number_map {
+    ( $func_name:ident, $x:expr ) => {
+        fn $func_name() -> &'static HashMap<&'static str, char> {
+            static HASHMAP: OnceLock<HashMap<&'static str, char>> = OnceLock::new();
+            HASHMAP.get_or_init(|| {
+                let number_map = $x
+                    //.map(|s| s.to_string())
+                    .into_iter()
+                    .zip((0..$x.len()).map(|i| char::from_digit(i as u32, 10).unwrap()));
+
+                HashMap::from_iter(number_map)
+            })
+        }
+    };
+}
+
+// {"o","on","one","t","tw","two","t","th", ...}
+macro_rules! make_set_incremental_str {
+    ( $func_name:ident, $x:expr ) => {
+        fn $func_name() -> &'static HashSet<&'static str> {
+            static HASHMAP: OnceLock<HashSet<&str>> = OnceLock::new();
+            HASHMAP.get_or_init(|| {
+                let number_str_sequential_letters = $x
+                    .iter()
+                    .flat_map(|&s| (1..=s.len()).map(|i| &s[0..i]).collect::<Vec<&str>>());
+                HashSet::from_iter(number_str_sequential_letters)
+            })
+        }
+    };
+}
+
+// {'z', 'o', 't', 't', 'f', 'f', 's', 's', 'e', 'n'}
+macro_rules! make_first_number_char {
+    ( $func_name:ident, $x:expr ) => {
+        fn $func_name() -> &'static HashSet<char> {
+            static HASHMAP: OnceLock<HashSet<char>> = OnceLock::new();
+            HASHMAP.get_or_init(|| {
+                let number_str_first_letter_char = $x.map(|s| s.chars().next().unwrap());
+                HashSet::from_iter(number_str_first_letter_char)
+            })
+        }
+    };
+}
+
+make_number_set!(number_set, NUMBERS);
+make_number_set!(number_set_rev, NUMBERS_REV);
+
+make_number_map!(number_map, NUMBERS);
+make_number_map!(number_map_rev, NUMBERS_REV);
+
+make_set_incremental_str!(number_set_incremental_str, NUMBERS);
+make_set_incremental_str!(number_set_incremental_str_rev, NUMBERS_REV);
+
+make_first_number_char!(number_first_char_mapping, NUMBERS);
+make_first_number_char!(number_first_char_mapping_rev, NUMBERS_REV);
+
+// eight1234eight == 88
+// oneight == 11
+fn extract_digits_v2(strs: Vec<String>) -> u32 {
+    let mut all_nums: Vec<u32> = vec![];
+
+    for s in strs {
+        let mut nums: Vec<char> = vec![];
+        let mut buf: String = String::new();
+
+        for c in s.chars() {
+            if c.is_numeric() {
+                nums.push(c);
+            } else if {
+                let extended_str = format!("{}{}", buf, c);
+                number_set_incremental_str().contains(&extended_str[..])
+            } {
+                buf.push(c);
+                if number_set().contains(&buf[..]) {
+                    nums.push(*number_map().get(&buf[..]).unwrap());
+                }
+            } else if number_first_char_mapping().contains(&c) {
+                buf.clear();
+                buf.push(c);
+            }
+        }
+
+        let first_digit = nums.first().unwrap();
+        let last_digit = nums.last().unwrap();
+        let number: u32 = String::from_iter([first_digit, last_digit])
+            .parse()
+            .unwrap();
+        all_nums.push(number);
+    }
+
+    println!("{:?}", all_nums);
+    all_nums.iter().sum::<u32>()
+}
+
+// eight1234eight == 88
+// oneight == 18
+fn extract_digits_v3(strs: Vec<String>) -> u32 {
+    let mut nums: Vec<u32> = vec![];
+
+    for s in strs {
+        let mut buf = String::new();
+        let mut chars = s.chars();
+        let mut chars_rev = s.chars().rev();
+
+        let first_digit = loop {
+            let c = chars.next().expect("No digits were found.");
+
+            if c.is_numeric() {
+                break c;
+            } else if {
+                let extended_str = format!("{}{}", buf, c);
+                number_set_incremental_str().contains(&extended_str[..])
+            } {
+                buf.push(c);
+                if number_set().contains(&buf[..]) {
+                    break *number_map().get(&buf[..]).unwrap();
+                }
+            } else if number_first_char_mapping().contains(&c) {
+                buf.clear();
+                buf.push(c);
+            }
+        };
+
+        let last_digit = loop {
+            let c = chars_rev.next().expect("No digits were found.");
+
+            if c.is_numeric() {
+                break c;
+            } else if {
+                let extended_str = format!("{}{}", buf, c);
+                number_set_incremental_str_rev().contains(&extended_str[..])
+            } {
+                buf.push(c);
+                if number_set_rev().contains(&buf[..]) {
+                    break *number_map_rev().get(&buf[..]).unwrap();
+                }
+            } else if number_first_char_mapping_rev().contains(&c) {
+                buf.clear();
+                buf.push(c);
+            }
+        };
+
+        let number: u32 = String::from_iter([first_digit, last_digit])
+            .parse()
+            .unwrap();
+        nums.push(number);
+    }
+
+    //println!("{:?}", nums);
+    nums.iter().sum::<u32>()
+}
+
+// eight1234eight == 14
 fn extract_digits(strs: Vec<String>) -> u32 {
     let mut nums: Vec<u32> = vec![];
 
     for s in strs {
-        println!("{s}");
-
         let mut chars = s.chars();
         let mut chars_rev = s.chars().rev();
 
@@ -101,9 +233,29 @@ pub fn trebuchet(s: &str) -> u32 {
             );
             extract_digits(strs)
         }
+        "example2" => {
+            let strs = parse_text(
+                "/Users/porlando/Projects/rust/advent_of_code_23/tests/day1_example2.txt",
+            );
+            extract_digits_v2(strs)
+        }
+        "example3" => {
+            let strs = parse_text(
+                "/Users/porlando/Projects/rust/advent_of_code_23/tests/day1_example2.txt",
+            );
+            extract_digits_v3(strs)
+        }
         "actual" => {
             let strs = parse_text("/Users/porlando/Projects/rust/advent_of_code_23/tests/day1.txt");
             extract_digits(strs)
+        }
+        "actual2" => {
+            let strs = parse_text("/Users/porlando/Projects/rust/advent_of_code_23/tests/day1.txt");
+            extract_digits_v2(strs)
+        }
+        "actual3" => {
+            let strs = parse_text("/Users/porlando/Projects/rust/advent_of_code_23/tests/day1.txt");
+            extract_digits_v3(strs)
         }
         _ => 0,
     }
@@ -120,16 +272,30 @@ mod tests {
     }
 
     #[test]
-    fn test_actual() {
-        assert_eq!(trebuchet("actual"), 55447)
+    fn test_v2() {
+        assert_eq!(trebuchet("example2"), 281);
     }
 
     #[test]
-    fn test_parse_text() {
-        let fpath: &str = "/Users/porlando/Projects/rust/advent_of_code_23/tests/day1_example.txt";
-        let expected: [String; 4] =
-            ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"].map(String::from);
-        assert_eq!(parse_text(fpath), expected)
+    fn test_v3() {
+        assert_eq!(trebuchet("example3"), 281);
+    }
+
+    #[test]
+    fn test_actual() {
+        assert_eq!(trebuchet("actual"), 55447);
+    }
+
+    #[test]
+    fn test_actual_v2() {
+        assert!(trebuchet("actual2") > 54569);
+        assert!(trebuchet("actual2") < 54729);
+    }
+
+    #[test]
+    fn test_actual_v3() {
+        assert!(trebuchet("actual3") > 54569);
+        assert!(trebuchet("actual3") < 54729);
     }
 
     #[test]
@@ -139,5 +305,13 @@ mod tests {
             .to_vec();
         let expected: u32 = 142;
         assert_eq!(extract_digits(input_strs), expected)
+    }
+
+    #[test]
+    fn test_parse_text() {
+        let fpath: &str = "/Users/porlando/Projects/rust/advent_of_code_23/tests/day1_example.txt";
+        let expected: [String; 4] =
+            ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"].map(String::from);
+        assert_eq!(parse_text(fpath), expected)
     }
 }
--- advent_of_code_23/src/main.rs
+++ advent_of_code_23/src/main.rs
@@ -1,8 +1,11 @@
 pub mod day1;
 
 fn main() {
-    let day1_result_example = day1::trebuchet("example");
-    println!("{day1_result_example}");
-    let day1_result = day1::trebuchet("actual");
-    println!("{day1_result}");
+    //let day1_result = day1::trebuchet("actual");
+    //println!("{day1_result}");
+    //let day1_result_2 = day1::trebuchet("actual3");
+    //println!("{day1_result_2}");
+
+    let result = day1::trebuchet("example2");
+    println!("{result}");
 }
