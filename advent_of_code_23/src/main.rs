48bf567d2530c3e656a706c7eb6b45194f127249
Pedro Orlando <porlando@riseup.net>
2024-11-26 19:22:50 -0300

    [AdvOfCode] Improving main module

--- advent_of_code_23/src/main.rs
+++ advent_of_code_23/src/main.rs
@@ -1,11 +1,25 @@
 pub mod day1;
+pub mod day2;
 
+use std::io::stdin;
 fn main() {
-    //let day1_result = day1::trebuchet("actual");
-    //println!("{day1_result}");
-    //let day1_result_2 = day1::trebuchet("actual3");
-    //println!("{day1_result_2}");
+    loop {
+        let mut input = String::new();
+        println!("Choose a Day from {} to {}; 0 exits.", 1, 1);
+        let input = match stdin().read_line(&mut input) {
+            Ok(_) => input,
+            Err(_) => panic!("Unexpected input."),
+        }
+        .chars()
+        .nth(0)
+        .unwrap_or('0')
+        .to_digit(10)
+        .unwrap_or(0);
 
-    let result = day1::trebuchet("example2");
-    println!("{result}");
+        match input {
+            0 => break,
+            1 => println!("{}", day1::trebuchet("actual")),
+            _ => todo!(),
+        }
+    }
 }
