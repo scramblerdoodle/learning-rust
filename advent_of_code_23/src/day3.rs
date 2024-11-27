585ebaffc7db93b181e6c02f8b86ae6a25bdb0f9
Pedro Orlando <porlando@riseup.net>
2024-11-27 12:51:12 -0300

    [AdvOfCode][Day 3] Starting process function

--- advent_of_code_23/src/day3.rs
+++ advent_of_code_23/src/day3.rs
@@ -9,13 +9,53 @@
 mod schematic_processor {
     use std::fs;
 
+    pub trait SpecialCharacter {
+        fn is_special_character(c: char) -> bool;
+    }
+
+    impl SpecialCharacter for char {
+        fn is_special_character(c: char) -> bool {
+            c != '.' && !c.is_alphanumeric()
+        }
+    }
+    //  Input: filename
+    //  Return example:
+    //      Vec[
+    //      [ 467..114.. ]
+    //      [ ...*...... ]
+    //      [ ..35..633. ]
+    //      [ ......#... ]
+    //      [ 617*...... ]
+    //      [ .....+.58. ]
+    //      [ ..592..... ]
+    //      [ ......755. ]
+    //      [ ...$.*.... ]
+    //      [ .664.598.. ]
+    //      ]
+    //  Where all of these are chars
     fn build_matrix(filename: &str) -> Vec<Vec<char>> {
         let contents = fs::read_to_string(filename).unwrap();
         Vec::from_iter(contents.lines().map(|l| l.chars().collect()))
     }
 
     pub fn process(filename: &str) -> i32 {
-        // let m = build_matrix(filename);
+        let m = build_matrix(filename);
+        for i in 0..m.len() {
+            println!("{:?}", m[i]);
+            for j in 0..m[i].len() {
+                if char::is_special_character(m[i][j]) {
+                    // check surroundings, whole schbang about not going out of bounds etc
+                    // if a digit is found, magic stuff will happen
+                    //      > how do check the full digit, cant repeat it
+                    //      > idea: skip if middle of number?
+                    //      > better idea: keep a different matrix keeping track of saved digits
+                    //      >           e.g. 4 6 7        1 1 1
+                    //                       . # .   =>   0 ? 0
+                    //                       1 4 6        0 0 0
+                    //                  after finding 467 it's marked as 1
+                }
+            }
+        }
         4361
     }
 }
@@ -36,5 +76,18 @@ mod tests {
     #[test]
     fn test_example() {
         assert_eq!(decipher_schematic("example"), 4361);
+    }
+
+    #[test]
+    fn is_special_character() {
+        // Supposed to be a special character
+        assert!(<char as schematic_processor::SpecialCharacter>::is_special_character('@'));
+        assert!(<char as schematic_processor::SpecialCharacter>::is_special_character('#'));
+        assert!(<char as schematic_processor::SpecialCharacter>::is_special_character('*'));
+
+        // Not supposed to be a special character
+        assert!(!<char as schematic_processor::SpecialCharacter>::is_special_character('a'));
+        assert!(!<char as schematic_processor::SpecialCharacter>::is_special_character('1'));
+        assert!(!<char as schematic_processor::SpecialCharacter>::is_special_character('.'));
     }
 }
