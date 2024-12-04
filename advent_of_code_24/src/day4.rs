ecf87123528d790cf0bbc6011599ffffc8c3a437
Pedro Orlando <porlando@riseup.net>
2024-12-04 13:20:01 -0300

    [AdvOfCode24][Day 4] Part 1 done

--- advent_of_code_24/src/day4.rs
+++ advent_of_code_24/src/day4.rs
@@ -1,7 +1,71 @@
 use std::fs::read_to_string;
 
+const XMAS: [char; 4] = ['X', 'M', 'A', 'S'];
+
+const DIRECTIONS: [(i16, i16); 8] = [
+    (-1, -1), // Top Left
+    (-1, 0),  // Top
+    (-1, 1),  // Top Right
+    (0, -1),  // Left
+    (0, 1),   // Right
+    (1, -1),  // Down Left
+    (1, 0),   // Down
+    (1, 1),   // Down Right
+];
+
+fn check_match(board: &Vec<Vec<char>>, i: usize, j: usize, d: (i16, i16), l: usize) -> bool {
+    let next_i = i as i16 + d.0;
+    let next_j = j as i16 + d.1;
+
+    if l == XMAS.len() {
+        return true;
+    }
+
+    if next_i < 0 || next_j < 0 {
+        return false;
+    }
+
+    let next_i = next_i as usize;
+    let next_j = next_j as usize;
+
+    match board.get(next_i) {
+        None => false, // out of bounds
+        Some(v) => {
+            match v.get(next_j) {
+                None => false, // out of bounds
+                Some(c) => {
+                    // Got a match for next letter
+                    if *c == XMAS[l] {
+                        // Repeat for next letter in next position
+                        check_match(board, next_i, next_j, d, l + 1)
+                    } else {
+                        false
+                    }
+                }
+            }
+        }
+    }
+}
+
 fn match_crosswords(board: Vec<Vec<char>>) -> u32 {
-    0
+    let mut matches: u32 = 0;
+
+    // for every element in board
+    for i in 0..board.len() {
+        for j in 0..board[i].len() {
+            // if it's X
+            if board[i][j] == XMAS[0] {
+                // check surroundings
+                for d in DIRECTIONS {
+                    matches += match check_match(&board, i, j, d, 1) {
+                        true => 1,
+                        false => 0,
+                    };
+                }
+            }
+        }
+    }
+    matches
 }
 
 fn match_crosswords_v2(board: Vec<Vec<char>>) -> u32 {
