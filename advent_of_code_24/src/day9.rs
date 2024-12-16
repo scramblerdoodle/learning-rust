231b9b3d13e6fc30ef0570797ccf24f36ba11d78
Pedro Orlando <porlando@riseup.net>
2024-12-16 18:34:48 -0300

    [AdvOfCode24][Day 9] Part 1 failed

--- advent_of_code_24/src/day9.rs
+++ advent_of_code_24/src/day9.rs
@@ -26,25 +26,52 @@ impl Input {
         }
     }
 
-    fn push_n(&mut self, s: String, n: u32) {
+    fn push_n(&mut self, s: String, n: u64) {
         for _ in 0..n {
             self.file_blocks.push_str(&s[..]);
         }
     }
 }
 
-fn day9(input: Input) -> Result<u32, ParseIntError> {
-    let mut result: u32 = 0;
-    // for input in inputs {
-    //     result += input.file_blocks.parse::<u32>()?;
-    // }
+fn day9(input: Input) -> Result<u64, ParseIntError> {
+    let mut result: u64 = 0;
+    let mut files: Vec<char> = input.file_blocks.trim_matches('.').chars().collect();
+
+    let mut j: usize = files.len() - 1;
+
+    for i in 0..files.len() {
+        // println!("i: {i}, j: {j}");
+        // println!("{:?}", String::from_iter(&files));
+        if files[i] == '.' {
+            files[i] = files[j];
+            files[j] = '.';
+            while files[j] == '.' {
+                j -= 1;
+            }
+        }
+
+        if i >= j {
+            break;
+        }
+    }
+
+    println!("{:?}", String::from_iter(&files));
+
+    let blocks: Vec<u64> = files
+        .iter()
+        .filter(|c| **c != '.')
+        .map(|c| c.to_digit(10).unwrap().into())
+        .collect();
+    for i in 0..blocks.len() {
+        result += (i as u64) * blocks[i];
+    }
     Ok(result)
 }
 
-fn day9_v2(input: Input) -> Result<u32, ParseIntError> {
-    let mut result: u32 = 0;
+fn day9_v2(input: Input) -> Result<u64, ParseIntError> {
+    let mut result: u64 = 0;
     // for input in inputs {
-    //     result += input.file_blocks.parse::<u32>()?;
+    //     result += input.file_blocks.parse::<u64>()?;
     // }
     Ok(result)
 }
@@ -52,7 +79,7 @@ fn day9_v2(input: Input) -> Result<u32, ParseIntError> {
 fn parse_input(filepath: &str) -> Input {
     let mut state: State = State::FILE;
     let mut result: Input = Input::new();
-    let mut file_id: u32 = 0;
+    let mut file_id: u64 = 0;
     read_to_string(filepath)
         .unwrap()
         .trim()
@@ -61,10 +88,10 @@ fn parse_input(filepath: &str) -> Input {
             let d = c.to_digit(10).unwrap();
             match state {
                 State::FILE => {
-                    result.push_n(file_id.to_string(), d);
+                    result.push_n(file_id.to_string(), d.into());
                     file_id += 1;
                 }
-                State::FREE => result.push_n(".".to_string(), d),
+                State::FREE => result.push_n(".".to_string(), d.into()),
             }
             state = state.change_state();
         });
@@ -73,7 +100,7 @@ fn parse_input(filepath: &str) -> Input {
     result
 }
 
-pub fn main(s: &str) -> u32 {
+pub fn main(s: &str) -> u64 {
     let result = match s {
         "example" => day9(parse_input("./tests/day9/example.txt")),
         "actual" => day9(parse_input("./tests/day9/actual.txt")),
@@ -90,7 +117,7 @@ mod tests {
 
     #[test]
     fn test_example() {
-        assert_eq!(main("example"), 10);
+        assert_eq!(main("example"), 1928);
     }
 
     #[test]
@@ -104,5 +131,10 @@ mod tests {
     #[test]
     fn test_example_v2() {
         assert_eq!(main("example_v2"), 0);
+    }
+
+    #[test]
+    fn test_actual() {
+        assert!(main("actual") > 91337315408);
     }
 }
