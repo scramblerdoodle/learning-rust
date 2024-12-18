94ea7b7b3efcdcb22ccbae32c1ccf7f0ed83368f
Pedro Orlando <porlando@riseup.net>
2024-12-18 15:06:40 -0300

    [AdvOfCode24][Day 9] The worst code that was ever written (Part 1)

--- advent_of_code_24/src/day9.rs
+++ advent_of_code_24/src/day9.rs
@@ -1,5 +1,7 @@
+use std::fmt;
 use std::{fs::read_to_string, num::ParseIntError};
 
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum State {
     FILE,
     FREE,
@@ -14,38 +16,135 @@ impl State {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Block {
+    id: Option<usize>,
+    state: State,
+    size: usize,
+}
+
+impl Block {
+    fn to_string(&self) -> String {
+        match self.state {
+            State::FREE => (0..self.size).map(|_| ".").collect(),
+            State::FILE => (0..self.size)
+                .map(|_| self.id.unwrap().to_string())
+                .collect(),
+        }
+    }
+
+    fn is_free(&self) -> bool {
+        match self.state {
+            State::FREE => true,
+            State::FILE => false,
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Input {
-    file_blocks: String,
+    file_blocks: Vec<Block>,
 }
 
 impl Input {
     fn new() -> Self {
         Self {
-            file_blocks: String::new(),
+            file_blocks: Vec::new(),
         }
     }
 
-    fn push_n(&mut self, s: String, n: u64) {
-        for _ in 0..n {
-            self.file_blocks.push_str(&s[..]);
+    fn push_block(&mut self, b: Block) {
+        self.file_blocks.push(b);
+    }
+}
+
+impl fmt::Display for Input {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.file_blocks.to_string())
+    }
+}
+
+trait SliceExt {
+    fn trim_r(&self) -> &Self;
+}
+
+impl SliceExt for [Block] {
+    fn trim_r(&self) -> &Self {
+        if let Some(last) = self.iter().rposition(|c| c.state != State::FREE) {
+            &self[0..last + 1]
+        } else {
+            unreachable!();
         }
     }
 }
 
+trait ToString {
+    fn to_string(&self) -> String;
+}
+impl ToString for Vec<Block> {
+    fn to_string(&self) -> String {
+        let s = self.iter().map(|b| b.to_string());
+        String::from_iter(s)
+    }
+}
+
 fn day9(input: Input) -> Result<u64, ParseIntError> {
-    let mut result: u64 = 0;
-    let mut files: Vec<char> = input.file_blocks.trim_matches('.').chars().collect();
+    // 0...11..2222
+    // becomes
+    //  Block{id:0, size:1},
+    //  Block{id:None, size:3},
+    //  Block{id:1, size:2},
+    //  Block{id:None, size:2},
+    //  Block{id:2, size:4}
+    // which in turn will become
+    //
+    // 0222112.....
+    //  Block{id:0, size:1},
+    //  Block{id:2, size:3},
+    //  Block{id:1, size:2},
+    //  Block{id:2, size:1},
+    //  Block{id:None, size:5}
 
-    let mut j: usize = files.len() - 1;
+    // 0...11..22
+    // becomes
+    //  Block{id:0, size:1},
+    //  Block{id:None, size:3},
+    //  Block{id:1, size:2},
+    //  Block{id:None, size:2},
+    //  Block{id:2, size:2}
+    // which in turn will become
+    //
+    // 02211.....
+    //  Block{id:0, size:1},
+    //  Block{id:2, size:2},
+    //  Block{id:1, size:2},
+    //  Block{id:None, size:5}
+    let mut blocks: Vec<Block> = input.file_blocks.trim_r().to_vec();
 
-    for i in 0..files.len() {
+    let mut j: usize = blocks.len() - 1;
+
+    for mut i in 0..blocks.len() {
         // println!("i: {i}, j: {j}");
-        // println!("{:?}", String::from_iter(&files));
-        if files[i] == '.' {
-            files[i] = files[j];
-            files[j] = '.';
-            while files[j] == '.' {
+        // println!("{:?}", blocks.to_string());
+
+        if blocks[i].is_free() {
+            if blocks[i].size < blocks[j].size {
+                blocks[i].id = blocks[j].id;
+                blocks[i].state = State::FILE;
+                blocks[j].size -= blocks[i].size;
+            } else if blocks[i].size > blocks[j].size {
+                blocks[i].size -= blocks[j].size;
+                let block = blocks[j];
+                blocks[j].state = State::FREE;
+                blocks[j].id = None;
+                blocks.insert(i, block);
+                i += 1;
+            } else {
+                let block = blocks[j];
+                blocks[j] = blocks[i];
+                blocks[i] = block;
+            }
+            while blocks[j].is_free() {
                 j -= 1;
             }
         }
@@ -55,15 +154,15 @@ fn day9(input: Input) -> Result<u64, ParseIntError> {
         }
     }
 
-    println!("{:?}", String::from_iter(&files));
+    // println!("{:?}", blocks.to_string());
 
-    let blocks: Vec<u64> = files
-        .iter()
-        .filter(|c| **c != '.')
-        .map(|c| c.to_digit(10).unwrap().into())
-        .collect();
-    for i in 0..blocks.len() {
-        result += (i as u64) * blocks[i];
+    let mut result: u64 = 0;
+    let mut pos: usize = 0;
+    for b in blocks.trim_r() {
+        for _ in 0..b.size {
+            result += (pos as u64) * (b.id.unwrap() as u64);
+            pos += 1;
+        }
     }
     Ok(result)
 }
@@ -79,7 +178,7 @@ fn day9_v2(input: Input) -> Result<u64, ParseIntError> {
 fn parse_input(filepath: &str) -> Input {
     let mut state: State = State::FILE;
     let mut result: Input = Input::new();
-    let mut file_id: u64 = 0;
+    let mut file_id: usize = 0;
     read_to_string(filepath)
         .unwrap()
         .trim()
@@ -88,15 +187,23 @@ fn parse_input(filepath: &str) -> Input {
             let d = c.to_digit(10).unwrap();
             match state {
                 State::FILE => {
-                    result.push_n(file_id.to_string(), d.into());
+                    result.push_block(Block {
+                        id: Some(file_id),
+                        size: d as usize,
+                        state: State::FILE,
+                    });
                     file_id += 1;
                 }
-                State::FREE => result.push_n(".".to_string(), d.into()),
+                State::FREE => result.push_block(Block {
+                    id: None,
+                    size: d as usize,
+                    state: State::FREE,
+                }),
             }
             state = state.change_state();
         });
 
-    println!("{:?}", result);
+    // println!("{:?}", result);
     result
 }
 
@@ -123,18 +230,15 @@ mod tests {
     #[test]
     fn test_input() {
         assert_eq!(
-            parse_input("./tests/day9/example.txt").file_blocks,
-            "00...111...2...333.44.5555.6666.777.888899"
+            parse_input("./tests/day9/example.txt")
+                .file_blocks
+                .to_string(),
+            "00...111...2...333.44.5555.6666.777.888899".to_string()
         );
     }
 
     #[test]
     fn test_example_v2() {
         assert_eq!(main("example_v2"), 0);
-    }
-
-    #[test]
-    fn test_actual() {
-        assert!(main("actual") > 91337315408);
     }
 }
